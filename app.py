618880faa9fb95ea9894980bbd1ef330140d58cd
Behser Aj <besher.aj@outlook.com>
2020-09-30 00:37:08 +0800

    login and signup already added tested and working perfectly

--- app.py
+++ app.py
@@ -22,30 +22,57 @@ def index():
 @app.route('/signup', methods=['POST', 'GET'])
 def signup():
     if request.method == 'POST':
+        email = request.form["email"].lower()
+        name = request.form["name"].lower()
         users = mongo.db.users
-        existing_user = users.find_one({'email' : request.form['email']})
+        existing_user = users.find_one({'email' : email})
         if existing_user:
             flash("Email already exists")
             return redirect(url_for("signup"))
 
         if existing_user is None:
             hashpass = generate_password_hash(request.form['pass'])
-            users.insert({'name' : request.form['name'].lower(), 
+            users.insert({'name' : name, 
             'password' : hashpass,
-            'email' : request.form['email'].lower()})
-            session['email'] = request.form['email']
+            'email' : email})
+            session['email'] = email
             session['logged_in'] = True
-            return redirect(url_for('profile', email=request.form['email']))
-        
+            return redirect(url_for('profile', email=email))        
     return render_template("signup.html")
 
-@app.route('/login')
+@app.route('/login',methods=['POST', 'GET'])
 def login():
-    return render_template("login.html")
+    if request.method=="POST":
+        existing_user = mongo.db.users.find_one(
+            {"email":request.form["email"].lower()})
+        if existing_user:
+            if check_password_hash(existing_user["password"], request.form["pass"]):
+                session["email"]    = request.form["email"]  
+                session['logged_in'] = True
+                return redirect(url_for('profile', email=request.form['email'].lower()))             
+            else:
+                flash("Incorrect username and/or password")
+                return redirect(url_for("login"))   
+        else:
+            flash("The user doesn't exist")
+            return redirect (url_for("login"))
+    return render_template("login.html")           
 
 @app.route('/profile/<string:email>')
 def profile(email):
-    return render_template("profile.html",name = request.form['name'])
+    if session["email"].lower() == email.lower():
+        existing_user = mongo.db.users.find_one(
+            {"email":email.lower()})
+        return render_template("profile.html",email=email, name=existing_user["name"])
+    else:
+        flash("You are not allowed to access someone's else profile")
+        return render_template("forbidden.html")
+
+@app.route('/logout')
+def logout():
+    session.pop('email');
+    session.pop('logged_in')
+    return render_template('login.html');
 
 
 
